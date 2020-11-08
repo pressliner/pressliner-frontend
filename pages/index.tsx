3f877673f72b5ae3b37b139f4be7b0b72cfc3fcc
Your Name <lucas.farias@mercadolibre.com>
2020-11-08 02:31:36 -0300

    Layout working

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,14 @@ const Home = () => {
     <Layout>
       <div className={styles.container}>
         <header className={styles.header}>
-          <div>Pressliner</div>
+          <div className={styles.location}>Buenos Aires City, Argentina</div>
+          <div className={styles.logo}>
+            Pressliner <div className={styles.slogan}>The modern newspaper</div>
+          </div>
+
+          <div className={styles.weather}>Weather: 19 C. Mostly Sunny</div>
         </header>
-        <main className={styles.main}>Welcome, User.</main>
+        <main className={styles.main}>Headline</main>
       </div>
     </Layout>
   );
